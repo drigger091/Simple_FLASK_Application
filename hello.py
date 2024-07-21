0be0892ad492953a9287c2c1b14a75df7c72d0be
drigger091 <42235258+drigger091@users.noreply.github.com>
2024-07-21 16:52:28 +0530

    update flask file

--- hello.py
+++ hello.py
@@ -1,11 +1,49 @@
-from flask import Flask ,render_template,url_for
+from flask import Flask ,render_template,url_for ,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+def home():
+    return "<h2>Hello ,communities</h2>"
 
-def hello_world():
-    return render_template('index.html')
+@app.route("/",methods =["GET"])
+def welcome():
+    return 'Welcome to my flask app by Dipayan Bhowal'
+
+@app.route("/index",methods=["GET"])
+def index():
+    return render_template("index.html")
+
+# varibale rule   
+@app.route('/sucess/<int:score>')
+def sucess(score):
+    return" The person has passed and the score is:"+ str(score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return" The person has failed and the score is:"+ str(score)
+
+@app.route('/calculate',methods =["GET","POST"])
+def form():
+    if request.method == "GET":
+        return render_template("calculate.html")
+    else:
+        maths = float(request.form['maths'])
+        science = float(request.form['science'])
+        history = float(request.form['history'])
+
+        average_marks = (maths + science + history)/3
+        result = ""
+        if average_marks < 50:
+            result = "sucess"
+        else:
+            result = "fail"
+
+
+        return render_template("results.html",result = average_marks)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
